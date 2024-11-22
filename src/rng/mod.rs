2ae9444e71bf8c85d34318d80a84c9b422c3f122
Laurent Fazio <laurent.fazio@gmail.com>
2024-11-22 09:46:40 +0100

    rng: add u32 range random generation
    Signed-off-by: Laurent Fazio <laurent.fazio@gmail.com>

--- src/rng/mod.rs
+++ src/rng/mod.rs
@@ -7,3 +7,7 @@ pub fn random() -> f64 {
 pub fn random_range(min: f64, max: f64) -> f64 {
     min + (max - min) * random()
 }
+
+pub fn random_range_u32(min: u32, max: u32) -> u32 {
+    ((min + (max - min)) as f64 * random()) as u32
+}
